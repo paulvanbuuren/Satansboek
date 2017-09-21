8316833bff7ba3d3d7fa7d7a963ac3a35502d13e
Paul van Buuren <paul@wbvb.nl>
2017-09-21 21:27:29 +0200

    wat CSS aanpassingen en gaan met de banaan

--- index.php
+++ index.php
@@ -1,10 +1,31 @@
 <?php
 
+define('STYLING_STYLESHEET', 'css/style.css' );
 
-  define('STYLING_STYLESHEET', 'css/style.css' );
+function sanitize_output($buffer) {
 
-?>  
-  
+    $search = array(
+        '/\>[^\S ]+/s',     // strip whitespaces after tags, except space
+        '/[^\S ]+\</s',     // strip whitespaces before tags, except space
+        '/(\s)+/s',         // shorten multiple whitespace sequences
+        '/<!--(.|\s)*?-->/' // Remove HTML comments
+    );
+
+    $replace = array(
+        '>',
+        '<',
+        '\\1',
+        ''
+    );
+
+    $buffer = preg_replace($search, $replace, $buffer);
+
+    return $buffer;
+}
+
+ob_start("sanitize_output");
+
+?>
 <!DOCTYPE html>
 <html lang="nl">
 <head lang="nl-NL" xml:lang="nl-NL">
@@ -47,8 +68,7 @@
 <meta property="og:title" content="Satansboek" />
 <meta property="og:type" content="article" />
 <meta property="og:url" content="https://satansboek.nl/" />
-
-	<title>satansboek.nl - WBVB Rotterdam</title>
+<title>satansboek.nl - WBVB Rotterdam</title>
 <?php
     echo '<style type="text/css">';
     include( STYLING_STYLESHEET );
@@ -57,22 +77,25 @@
 </head>
 
 <body>
-
 <article>
-<h1>Satansboek</h1>
-
-<svg width="190px" height="252px" viewBox="0 0 190 252" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
-  <title>Satansboek-logo</title>
-  <desc>Hier staat het Facebook-logo, licht aangepast door de toevoeging van twee duivelshoorntjes en een duivelsstaart</desc>
-  <defs></defs>
-  <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
-  <g id="satansboek" transform="translate(-36.000000, -16.000000)" fill-rule="nonzero" fill="#FFFFFF">
-  <g id="ef-you-too" transform="translate(36.000000, 16.000000)">
-  <path d="M36.1445405,220.480478 C48.2684664,222.252887 59.9374689,217.64077 67.28209,208.293071 C69.0155832,206.086807 70.2312009,203.455308 72.2039531,197.999644 C72.3356681,197.635384 72.8082211,196.322642 72.7999674,196.345539 C73.0453969,195.66469 73.2343789,195.144175 73.4229773,194.631292 C73.9141479,193.295582 74.3507482,192.15619 74.799421,191.053541 C78.2688771,182.527076 82.5364199,176.387232 89.5480245,171.411892 C94.2927061,168.04513 100.080998,165.471524 107.1,163.695564 L107.1,148.1 L72.3,148.1 L72.3,107.8 L107.1,107.8 L107.1,78.1 C107.1,68.6441566 108.68673,60.3670268 111.627571,53.3350727 L99.7220991,6.9507495 L129.53458,32.3190027 C136.752594,27.9606952 145.45232,25.4807288 155.24417,24.9904147 L173.530601,0.371874549 L172.925819,25.2001682 C178.627881,25.4625235 184.321841,25.8959328 190,26.5 L190,62.5 L168.7,62.5 C151.9,62.5 148.7,70.4 148.7,82.1 L148.7,107.8 L188.6,107.8 L183.4,148.1 L148.7,148.1 L148.7,251.4 L107.1,251.4 L107.1,176.151352 C102.85488,177.475262 99.3549125,179.167195 96.4923968,181.198398 C91.5943778,184.673966 88.5944689,188.990026 85.9144818,195.576308 C85.523311,196.537641 85.1333536,197.55531 84.6856452,198.772827 C84.5074101,199.257527 84.3263969,199.756094 84.0889027,200.414929 C84.0997854,200.384739 83.6240276,201.706385 83.4888469,202.080228 C81.0968707,208.69526 79.4842895,212.186083 76.71791,215.706929 C66.1810264,229.117509 49.1164889,235.344499 31.9746855,231.93706 L28.3535361,241.886086 L0.655210444,214.245787 L39.6402008,210.87623 L36.1445405,220.480478 Z" id="satansboek-wit"></path>
-  </g>
-  </g>
-  </g>
-</svg>
+  <div>
+    <h1>Satansboek</h1>
+    <p>Omdat Facebook de satanische satan is</p>
+  </div>
+  <div>
+    <svg width="190px" height="252px" viewBox="0 0 190 252" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+      <title>Satansboek-logo</title>
+      <desc>Hier staat het Facebook-logo, licht aangepast door de toevoeging van twee duivelshoorntjes en een duivelsstaart</desc>
+      <defs></defs>
+      <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+        <g id="satansboek" transform="translate(-36.000000, -16.000000)" fill-rule="nonzero" fill="#FFFFFF">
+          <g id="ef-you-too" transform="translate(36.000000, 16.000000)">
+            <path d="M36.1445405,220.480478 C48.2684664,222.252887 59.9374689,217.64077 67.28209,208.293071 C69.0155832,206.086807 70.2312009,203.455308 72.2039531,197.999644 C72.3356681,197.635384 72.8082211,196.322642 72.7999674,196.345539 C73.0453969,195.66469 73.2343789,195.144175 73.4229773,194.631292 C73.9141479,193.295582 74.3507482,192.15619 74.799421,191.053541 C78.2688771,182.527076 82.5364199,176.387232 89.5480245,171.411892 C94.2927061,168.04513 100.080998,165.471524 107.1,163.695564 L107.1,148.1 L72.3,148.1 L72.3,107.8 L107.1,107.8 L107.1,78.1 C107.1,68.6441566 108.68673,60.3670268 111.627571,53.3350727 L99.7220991,6.9507495 L129.53458,32.3190027 C136.752594,27.9606952 145.45232,25.4807288 155.24417,24.9904147 L173.530601,0.371874549 L172.925819,25.2001682 C178.627881,25.4625235 184.321841,25.8959328 190,26.5 L190,62.5 L168.7,62.5 C151.9,62.5 148.7,70.4 148.7,82.1 L148.7,107.8 L188.6,107.8 L183.4,148.1 L148.7,148.1 L148.7,251.4 L107.1,251.4 L107.1,176.151352 C102.85488,177.475262 99.3549125,179.167195 96.4923968,181.198398 C91.5943778,184.673966 88.5944689,188.990026 85.9144818,195.576308 C85.523311,196.537641 85.1333536,197.55531 84.6856452,198.772827 C84.5074101,199.257527 84.3263969,199.756094 84.0889027,200.414929 C84.0997854,200.384739 83.6240276,201.706385 83.4888469,202.080228 C81.0968707,208.69526 79.4842895,212.186083 76.71791,215.706929 C66.1810264,229.117509 49.1164889,235.344499 31.9746855,231.93706 L28.3535361,241.886086 L0.655210444,214.245787 L39.6402008,210.87623 L36.1445405,220.480478 Z" id="satansboek-wit"></path>
+          </g>
+        </g>
+      </g>
+    </svg>
+  </div>
 </article>
 </body>
 </html>
